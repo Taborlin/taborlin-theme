de1a947008b5a23c1dbc0cb170f9548ff8d819de
palmersp <palmersp@spencerpalmer.com>
2015-07-16 17:20:37 -0600

    adding a unit test

---
+++ test/themeTest.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+var fs = require('fs');
+
+function read(file) {
+  return fs.readFilySync(file, 'utf8');
+}
+
+describe('taborlin-theme', function() {
+  'use-strict';
+
+it('should create expected css file', function () {
+})
+})
